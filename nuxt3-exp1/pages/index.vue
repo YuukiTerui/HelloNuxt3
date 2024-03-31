321ed1fa8433948ade89b4bfac7ef88fcfccbc4d
YuukiTerui <teriyaki0839@gmail.com>
2024-03-31 17:10:02 +0900

    create exp1

---
+++ nuxt3-exp1/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Top Page</h1>
+        <NuxtLink to="/mypage">My Page</NuxtLink>
+    </div>
+</template>
---
+++ nuxt3-exp1/pages/mypage.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>My Page</h1>
+        <NuxtLink to="/">Top Page</NuxtLink>
+    </div>
+</template>
